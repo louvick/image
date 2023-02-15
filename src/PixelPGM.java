471d629bdcebd9386c8d750735bab8e166c4fd42
AntoPlouffe <monemail@gmail.com>
2023-02-15 15:28:08 -0500

    pixel class
    pixel class

---
+++ src/PixelPGM.java
@@ -0,0 +1,23 @@
+public class PixelPGM {
+
+    private int pixel;
+
+    PixelPGM(){
+
+        pixel = 0;
+    }
+    PixelPGM(int ppixel){
+
+        pixel = ppixel;
+    }
+
+    public int getPixel(){
+
+        return this.pixel;
+    }
+
+    public int setPixel(int ppixel){
+
+        return this.pixel;
+    }
+}
