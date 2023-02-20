c57d46edc512744a9fd27eb78220d61b3fa14108
louvick <louvick78@yahoo.com>
2023-02-20 13:58:25 -0500

    écrire

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         String chemin2= "image1.ppm";
         String chemin = "image1.pgm";
         imagecouleur.lire(chemin2);
-        imagecouleur.pivoter90();
-        im.ecrire("image1.ppm",imagecouleur);
+        //imagecouleur.pivoter90();
+        im.ecrire("image1.pgm",imagegrise);
 
         imagegrise.lire(chemin);
     }
