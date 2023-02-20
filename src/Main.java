7d6098a31a3af0ffa19d4a65efc8e5a71b55226e
louvick <louvick78@yahoo.com>
2023-02-20 13:34:52 -0500

    asd

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        ImageManager im = new ImageManager();
         ImagePGM imagegrise = new ImagePGM();
         ImagePPM imagecouleur = new ImagePPM();
         String chemin2= "image1.ppm";
         String chemin = "image1.pgm";
         imagecouleur.lire(chemin2);
         imagecouleur.pivoter90();
-        imagecouleur.ecrire("image1.ppm");
+        im.ecrire("image1.ppm",imagecouleur);
+
         imagegrise.lire(chemin);
     }
 }
