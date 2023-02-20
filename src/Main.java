7e7a3efbe9d4ae5079bc3aca77ec1edfdbc3747f
louvick <louvick78@yahoo.com>
2023-02-20 13:18:05 -0500

    asd

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         ImagePGM imagegrise = new ImagePGM();
         ImagePPM imagecouleur = new ImagePPM();
-        String chemin2= "fuckyou.ppm";
-        String chemin = "fuckyou.pgm";
+        String chemin2= "image1.ppm";
+        String chemin = "image1.pgm";
         imagecouleur.lire(chemin2);
+        imagecouleur.pivoter90();
+        imagecouleur.ecrire("image1.ppm",);
         imagegrise.lire(chemin);
     }
 }
