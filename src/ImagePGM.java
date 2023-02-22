884b421abbf35a1a916d167af1bf470a58aa56a7
AntoPlouffe <monemail@gmail.com>
2023-02-21 20:43:30 -0500

    Update ImagePGM.java

--- src/ImagePGM.java
+++ src/ImagePGM.java
@@ -341,7 +341,6 @@ public class ImagePGM extends Image{
      *
      * @author Antoine Plouffe, Louvick D'Arcy, Jean-François Labbé
      * @Date 20 février
-     * @param image
      * @return int
      *
      */
