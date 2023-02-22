522255ea30c42fb663167ea1513aa9e10c9740d6
AntoPlouffe <monemail@gmail.com>
2023-02-21 22:28:08 -0500

    main

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ public class Main {
 
 
         String chemin = "image1.pgm";
-        String chemin3 = "sadsadsadd.pgm";
         String chemin2 = "image1.ppm";
+        String originalPPM = "originalPPM.ppm";
+        String originalPGM = "originalPGM.pgm";
         String cheminextraire = "extraire.pgm";
         String cheminextraire2 = "extraire.ppm";
 
@@ -20,16 +21,25 @@ public class Main {
         ImagePPM imagecouleur = new ImagePPM();
         ImagePPM imagecouleur2 = new ImagePPM();
 
-        im.lire(imagecouleur,chemin2);
-        im.couleur_preponderante(imagecouleur);
 
+
+        //im.lire(imagecouleur, originalPPM);
+        //im.lire(imagegrise, originalPGM);
+
+        //test copier
+
+        //im.copier(imagegrise,imagegrise2);
+        //im.copier(imagecouleur, imagecouleur2);
+
+
+        //Test sont_identique
 /*
         im.lire(imagegrise,chemin);
         im.lire(imagegrise2,chemin);
         im.lire(imagecouleur,chemin2);
         im.lire(imagecouleur2,chemin2);
 
-        if(im.sont_identiques(imagecouleur2,imagegrise)) {
+        if(im.sont_identiques(imagegrise2,imagegrise)) {
             System.out.println("true");
         }
         else {
@@ -38,8 +48,42 @@ public class Main {
 
         im.ecrire("sadsadsadd.ppm",imagecouleur);
         im.ecrire("sadsadsadd.pgm",imagegrise);
-
 */
+
+
+        //Test pivoter90()
+
+        //im.pivoter90(imagecouleur);
+        //im.pivoter90(imagegrise);
+
+        //Test noircir_eclaircir
+
+        //im.eclaircir_noircir(imagecouleur, 100);
+        //im.eclaircir_noircir(imagegrise, 100);
+
+
+        //Test reduire
+
+        //im.reduire(imagecouleur);
+        //im.reduire(imagegrise);
+
+
+        //Test extraire
+
+        //im.extraire(imagecouleur,30,30,100,100);
+        //im.extraire(imagegrise,40,40,75,75);
+
+
+        //Test couleur_preponderante
+
+       // im.couleur_preponderante(imagecouleur);
+        //im.couleur_preponderante(imagegrise);
+
+
+
+        //Écriture
+        im.ecrire(chemin2, imagecouleur);
+        im.ecrire(chemin, imagegrise);
 
 
     }
